7e9b7f5008500ca58546f05b6df019f76ff43282
Jeden Zhan <msygzzy1998@gmail.com>
2023-10-31 18:13:10 +0800

    stash

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
-function solveEquation(equation) {
-  let factor = 0,
-    val = 0,
-    i = 0,
-    n = equation.length,
-    sign1 = 1;
-  while (i < n) {
-    if (equation[i] === "=") {
-      sign1 = -1;
-      i++;
-      continue;
-    }
-    let sign2 = sign1,
-      number = 0,
-      valid = false;
-    if (equation[i] === "-" || equation[i] === "+") {
-      sign2 = equation[i] === "-" ? -sign1 : sign1;
-      i++;
-    }
-    while (i < n && !isNaN(equation[i])) {
-      number = number * 10 + (equation[i].charCodeAt() - "0".charCodeAt());
-      i++;
-      valid = true;
-    }
-    if (i < n && equation[i] === "x") {
-      factor += valid ? sign2 * number : sign2;
-      i++;
-    } else {
-      val += sign2 * number;
+function packback(weight, value, w) {
+  const n = weight.length;
+  let max = [];
+  function dfs(i = 0, curry = [], left = w) {
+    // if (i === n || left === 0) max.push(curry.reduce((a, b) => a + b));
+    if (i === n || left === 0) max.push([...curry]);
+    else {
+      for (let j = i; j < n; j++) {
+        if (left - weight[j] < 0) continue;
+        curry.push(value[j]);
+        dfs(j + 1, curry, left - weight[j]);
+        curry.pop();
+      }
     }
   }
-  if (factor === 0) return val === 0 ? "Infinite solutions" : "No solution";
-  return `x=${-val / factor}`;
+
+  dfs();
+  console.log(max);
+  return Math.max(...max);
 }
 
-console.log(solveEquation("x+5-3+x=6+x-2"));
+console.log(packback([2, 2, 3, 1, 5, 2], [2, 3, 1, 5, 4, 3], 2));
